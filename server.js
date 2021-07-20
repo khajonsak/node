7788fca538fe7d855f147bb57a1e8221cb06d652
khajonsak <khajonsak.wo61@snru.ac.th>
2021-07-20 12:42:30 +0700

    ss

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get("/", (req, res)=>{
     res.json({result: "ok", data:[1,2,3,4,5]})
 })
 
+app.get("/s", (req, res)=>{
+    res.json({result: "ok", data:[1,2,3,4,5]})
+})
+
+
 app.listen(PORT, ()=>{
     console.log(`Serer is running. ${PORT}`)
 })

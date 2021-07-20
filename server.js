f9e97eb6c33d743f48eaa3a1ea09cd6e12d2d231
khajonsak <khajonsak.wo61@snru.ac.th>
2021-07-20 13:15:14 +0700

    ddd

--- server.js
+++ server.js
@@ -62,56 +62,6 @@ app.get('/api/v1/unlink-richmenu', (req, res) => {
 });
 
 
-app.post('/loginrichmenu', (req, res) => {
-    
-    const { username, password, userId } = req.body;
-    console.log(req.body);
-    if (username && password){
-        connection.query('SELECT * FROM user1  WHERE username = ? AND password = ?', [username, password], function(error, results, fields){
-     if(results.length > 0){ 
-         req.session.loggeedin = true;
-         req.session.userId = userId;
-         client.linkRichMenuToUser(userId, "richmenu-1753cf92786d4090d9590e8545094831");
-     
-         console.log('รหัสถูกต้อง')
-         res.end();
-     } else if (username && password) {
-        connection.query('SELECT * FROM user2 WHERE username = ? AND password = ?', [username, password], function(error, results, fields){
-            if(results.length > 0){ 
-                req.session.loggeedin = true;
-                req.session.userId = userId;
-                client.linkRichMenuToUser(userId, "richmenu-9819db4f8376aefb9a37f3da9e99bef2");
-          
-                console.log('รหัสถูกต้อง1')
-                res.end();
-                
-            } else if (username && password) {
-                connection.query('SELECT * FROM user3 WHERE username = ? AND password = ?', [username, password], function(error, results, fields){
-                    if(results.length > 0){ 
-                        req.session.loggeedin = true;
-                        req.session.userId = userId;
-                        client.linkRichMenuToUser(userId, "richmenu-5eadfafc7f550d4ea14db1a2ec76f56d");
-                  
-                        console.log('รหัสถูกต้อง2')
-                        res.end();
-                    } else {
-                      
-                        res.send('page 3')
-                        res.end();
-                    }
-
-                })
-            }
-        })
-        
-     } 
-     
- });
-
- }
-   
-})
-
 
 
 app.listen(PORT, ()=>{
